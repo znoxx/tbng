26a89f4b1df1c29f4fe266647a1b810493d9567a
znoxx <nzabrod@yandex.ru>
2017-05-10 16:26:22 +0300

    Initial clone of torstick-webui

---
+++ _startapp.bat
@@ -0,0 +1,1 @@
+set DEBUG=webui:* & npm start
---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var basicAuth = require('basic-auth-connect');
+
+
+var routes = require('./routes/index');
+var mode = require('./routes/mode');
+var system = require('./routes/system');
+var i2p = require('./routes/i2p');
+var wifi = require('./routes/wifi');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+var user=require("./config/user.json");
+app.use(basicAuth(user.username, user.password));  //TODO - Init from json
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/mode', mode);
+app.use('/system', system);
+app.use('/i2p', i2p);
+app.use('/wifi',wifi);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
