8e6098a3f2a4084ee981f1d6b1a7f271bd7f8f9b
znoxx <nzabrod@yandex.ru>
2017-07-06 12:36:20 +0200

    Testing bridge setting

--- routes/bridges.js
+++ routes/bridges.js
@@ -10,26 +10,29 @@ router.get('/', function(req, res, next) {
     //stub
     var modes= [];
     var someMode1 = {};
+    var someMode2 = {};
+    var someMode3 = {};
        
 
     someMode1.name="none";
     someMode1.current=true;
-    someMode1.value="";
+    
     modes.push(someMode1);
      
-//    modes[1].name="obfs3";
-//    modes[1].current=false;
- //   modes[1].value="obfs3 stub";
+    someMode2.name="obfs3";
+    someMode2.current=false;
+    someMode2.value="obfs3 stub";
+    modes.push(someMode2);
 
- //   modes[2].name="obfs4";
- //   modes[2].current=false;
- //   modes[2].value="obfs4 stub"
-    
+    someMode3.name="obfs4";
+    someMode3.current=false;
+    someMode3.value="obfs4 stub";
+    modes.push(someMode3);
 
  try{
      //interfaces = functions.getObfsModes();  
 
-    res.render('bridge', { title: 'Configure TOR bridges', available_modes: modes  });   
+    res.render('bridge', { title: 'Configure TOR bridges', available_modes: modes, data: "OBFS STUB"  });   
   }
   catch(e)
   {
@@ -44,6 +47,15 @@ router.post('/', function(req, res, next) {
   console.log(req.body.mode);
   try 
   {
+    
+     var bridges = req.body.data.split('\n');
+     var resultData ={};
+     resultData.mode=req.body.mode.trim();
+     resultData.bridges=[];
+     bridges.forEach(function(bridge,i) {
+        if(bridge.trim() != "") resultData.bridges.push(bridge.trim()); 
+     });
+     console.log(JSON.stringify(resultData));
     
      res.render('xresult', { title: 'Configure TOR bridges', message: 'TODO: Implement tor tor bridges' });
     //if ( typeof req.body.interface !== 'undefined' && req.body.interface )
