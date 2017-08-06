d114d3ae24c3acca440502b2a86a3624e8a00dc4
znoxx <nzabrod@yandex.ru>
2017-08-06 14:42:28 +0200

    Default plugin update

--- engine/plugins/macspoof_ifconfig.py
+++ engine/plugins/macspoof_ifconfig.py
@@ -10,6 +10,7 @@ def plugin_main(json_arguments=None):
     macaddr=$(printf '%02x' $((0x$(od /dev/urandom -N1 -t x1 -An | cut -c 2-) & 0xFE | 0x02)); od /dev/urandom -N5 -t x1 -An | sed 's/ /:/g')
     ifconfig {0} hw ether $macaddr
     ip link set {0} up
+    systemctl restart network-manager
     """.format(interface['name'])
     utility.run_multi_shell_command(command)
   else:
